773bc2ed71da5a533e58f090273b07032bf9b7ef
Aurora Dawn <131844170+StellarWitch7@users.noreply.github.com>
2023-05-20 01:01:42 -0400

    Create SkyDagger.cs

---
+++ Content/Items/Weapons/Melee/SkyDagger.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace StellaAurum.Content.Items.Weapons.Melee
+{
+    internal class SkyDagger : ModItem
+    {
+        public override void SetDefaults()
+        {
+            Item.damage = 54;
+            Item.DamageType = DamageClass.Melee;
+            Item.width = 40;
+            Item.height = 40;
+            Item.useTime = 5;
+            Item.useAnimation = 5;
+            Item.useStyle = ItemUseStyleID.Thrust;
+            Item.knockBack = 5;
+            Item.value = 1000;
+            Item.rare = ItemRarityID.Expert;
+            Item.UseSound = SoundID.Roar;
+            Item.expertOnly = true;
+            Item.autoReuse = true;
+            Item.crit = 12;
+        }
+
+        public override void AddRecipes()
+        {
+            Recipe recipe = CreateRecipe();
+            recipe.AddIngredient(ItemID.CopperShortsword, 1);
+            recipe.AddIngredient(ItemID.FragmentSolar, 50);
+            recipe.AddIngredient(ItemID.SilverBar, 15);
+            recipe.AddIngredient(ItemID.LunarBar, 20);
+            recipe.AddTile(TileID.LunarCraftingStation);
+            recipe.Register();
+        }
+    }
+}
